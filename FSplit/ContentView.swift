e3cce2beb1544696c2fc3923d6c6fe84a8f15ee7
Maxim Vainikka <rithmique@yahoo.com>
2021-04-20 20:11:22 +1000

    Update ContentView.swift

--- FSplit/ContentView.swift
+++ FSplit/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let tipArray = [10, 15, 20, 25, 0]
+    
+    @State private var tipSelected = 0
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    
+    var totalAmount: Double {
+        guard let doubleCheckAmount = Double(checkAmount) else { return 0.0 }
+        let convertedTipAmount = Double(tipArray[tipSelected]) / 100
+        let tipAmount = doubleCheckAmount * convertedTipAmount
+        
+        let result = (doubleCheckAmount + tipAmount)
+        return result
+    }
+    var totalAmountPerPerson: Double {
+        guard let convertedNumberOfPeople = Double(numberOfPeople) else { return totalAmount }
+        return totalAmount / convertedNumberOfPeople
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    
+                    TextField("Check amount: ", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    TextField("Enter number of people", text: $numberOfPeople).keyboardType(.numberPad)
+                }
+                Section(header: Text("How much tip you want to leave?")) {
+                    
+                    Picker("tip percentage", selection: $tipSelected) {
+                        ForEach(0..<tipArray.count) {
+                            
+                            Text("\(tipArray[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Total amount")) {
+                    
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                    
+                }
+                Section(header: Text("Amount per person")) {
+                    
+                    Text("$\(String(format: "%.2f", totalAmountPerPerson))")
+                }
+            }
+            .navigationBarTitle("FSplit")
+        }
     }
 }
 
