c49917441e163bc0b6819bd6c926fe0fd628eb17
Maxim Vainikka <rithmique@yahoo.com>
2021-04-20 18:44:14 +1000

    Initial Commit

---
+++ FSplit/FSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSplitApp.swift
+//  FSplit
+//
+//  Created by Максим Нуждин on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct FSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
